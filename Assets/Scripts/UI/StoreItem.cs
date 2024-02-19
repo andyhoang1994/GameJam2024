9e1d021c65494f909b126aea161e807527bef927
Andy <andyhoang@live.ca>
2024-02-18 22:28:48 -0800

    Add title and cost to store item

--- Assets/Scripts/UI/StoreItem.cs
+++ Assets/Scripts/UI/StoreItem.cs
@@ -1,9 +1,21 @@
+using TMPro;
 using UnityEngine;
 
 public class StoreItem : MonoBehaviour
 {
+    [SerializeField]
+    private string title;
+
+    [SerializeField]
+    private string cost;
     void Start()
     {
+        TextMeshProUGUI[] textComponents = this.GetComponentsInChildren<TextMeshProUGUI>();
+        TextMeshProUGUI title = textComponents[0];
+        TextMeshProUGUI cost = textComponents[1];
+
+        title.text = this.title;
+        cost.text = this.cost;
 
     }
 }
